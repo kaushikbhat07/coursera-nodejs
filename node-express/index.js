d1aafb6b27539142d63081ae3cc57f9cd1d0d916
Kaushik Bhat <kaushik.bhat11@learner.manipal.edu>
2020-05-15 12:45:24 +0530

    Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -1,21 +1,24 @@
 const express = require('express'),
 	http = require('http');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use('/dishes', dishRouter);
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname + '/public');
 
 app.use((req, res, next) => {
 	console.log(req.headers);
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
 
 const server = http.createServer(app);
 
